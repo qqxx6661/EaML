74127366a139e497dc4ce5c9b55434e4c9d85a3e
qqxx6661 <404013419@qq.com>
2017-11-03 16:08:36 -0600

    下一步： ML预测使用前面指定帧（暂时不弄） 修改论文图 改成动态

--- CLOUD_reID.py
+++ CLOUD_reID.py
@@ -40,7 +40,7 @@ def reID(filename, gallery_person_list):
     min_similiar = 99999
     for i in range(len(gallery_person_list)):
         gallery_hist = np.load(gallery_person_list[i])
-        # print(target_hist)
+        # print(len(target_hist))
         similiar = dist.cityblock(gallery_hist, target_hist)
         print(filename, gallery_person_list[i], similiar)
         if similiar == 0.0:  # 写入失败文件忽略
@@ -53,6 +53,6 @@ def reID(filename, gallery_person_list):
     return person
 
 
-# reID('BSU_data/0/61_0.npy', ['gallery/133_0.npy', 'gallery/60_0.npy'])
+reID('EDGE/BSU_data/0/61_0.npy', ['gallery/yolo_1547_61_0.npy', 'gallery/yolo_1547_143_0.npy'])
 
 # print(np.load('BSU_data/0/95_0.npy'))
